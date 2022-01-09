1fd9f627d52ecc737df94122a4991e68b9eb4b72
Erica L. Ingram <inquiries@aquoco.co>
2022-01-09 11:09:41 -0800

    feat: [] server setup

---
+++ API/server.spec.js
@@ -0,0 +1,27 @@
+const request = require(`supertest`);
+const server = require(`./server.js`);
+
+it(`should set db environment to testing`, () => {
+    expect(process.env.DB_ENV).toBe(`testing`);
+});
+
+describe(`server`, () => {
+    describe(`GET /`, () => {
+        it(`should return 200`, () => {
+            // run server & make a GET request to check for 200 response
+            return request(server)
+                .get(`/`)
+                .then(res => {
+                    expect(res.status).toBe(200);
+                });
+        });
+
+        it (`should return HTML`, () => {
+            return request(server)
+                .get(`/`)
+                .then(res => {
+                    expect(res.type).toMatch(/html/i);
+                });
+        })
+    })
+})

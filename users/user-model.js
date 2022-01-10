42f7e9d1e4848624490f04698b43e2a9b51fd5cb
Erica L. Ingram <inquiries@aquoco.co>
2022-01-09 19:43:21 -0800

    feat: [] added auth support

--- users/user-model.js
+++ users/user-model.js
@@ -1,5 +1,37 @@
 const db = require('../data/dbConfig');
 
+const find = () => {
+	return db('users').select('id', 'username', 'email');
+};
+
+const findBy = (filter) => {
+	return db('users').where(filter);
+};
+
+async function add (user) {
+	const [id] = await db('users').insert(user, 'id');
+	return findById(id);
+}
+
+const findById = (id) => {
+	return db('users')
+		.select('id', 'username', 'email')
+		.where({ id })
+		.first();
+};
+
+const update = (id, user) => {
+	return db('users')
+		.where('id', Number(id))
+		.update(user);
+};
+
+const remove = (id) => {
+	return db('users')
+		.where('id', Number(id))
+		.del();
+};
+
 module.exports = {
 	add,
 	find,
@@ -8,35 +40,3 @@ module.exports = {
 	update,
 	remove
 };
-
-function find () {
-	return db('users').select('id', 'username', 'email');
-}
-
-function findBy (filter) {
-	return db('users').where(filter);
-}
-
-async function add (user) {
-	const [id] = await db('users').insert(user, 'id');
-	return findById(id);
-}
-
-function findById (id) {
-	return db('users')
-		.select('id', 'username', 'email')
-		.where({ id })
-		.first();
-}
-
-function update (id, user) {
-	return db('users')
-		.where('id', Number(id))
-		.update(user);
-}
-
-function remove (id) {
-	return db('users')
-		.where('id', Number(id))
-		.del();
-}
